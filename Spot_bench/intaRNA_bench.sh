0c7223250789e2a8f3b6849982d2481bec0caa1d
Federico Di Marco <federico.dimarco91@gmail.com>
2021-03-22 10:54:33 +0100

    21_03_22

--- Spot_bench/intaRNA_bench.sh
+++ Spot_bench/intaRNA_bench.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir Predictions
+mkdir Raw
+
 ### Run IntaRNA sTar prediction for ryhB sRNA
 
 IntaRNA3 -q query/r.fasta -t target/NC_000913.fa --threads 4 --personality=IntaRNAsTar --outMode=C --out Predictions/res_tar_r.csv
@@ -12,12 +15,13 @@ IntaRNA3 -q query/s.fasta -t target/NC_000913.fa --threads 4 --personality=IntaR
 
 ### Run miranda prediction for ryhB sRNA
 
-miranda query/r.fasta target/NC_000913.fa -en -4 -out Predictions/miranda_r
+miranda query/r.fasta target/NC_000913.fa -en -4 -out Raw/miranda_r
 
-{ grep "Seq1,Seq2" Predictions/miranda_r | head -n 1; grep ">>" Predictions/miranda_r; } >> miranda_r
+{ grep "Seq1,Seq2" Raw/miranda_r | head -n 1 | sed 's/,/;/g'; grep ">>" Raw/miranda_r | sed 's/\t/;/g'| sed 's/ /_/g'; } > Predictions/miranda_r.csv
 
 ### Run miranda prediction for sgrS sRNA
 
-miranda query/r.fasta target/NC_000913.fa -en -4 -out Predictions/miranda_s
+miranda query/s.fasta target/NC_000913.fa -en -4 -out Raw/miranda_s
 
-{ grep "Seq1,Seq2" Predictions/miranda_s | head -n 1; grep ">>" Predictions/miranda_s; } >> miranda_s
+{ grep "Seq1,Seq2" Raw/miranda_s | head -n 1 | sed 's/,/;/g'; grep ">>" Raw/miranda_s | sed 's/\t/;/g'| sed 's/ /_/g'; } > Predictions/miranda_s.csv
+
